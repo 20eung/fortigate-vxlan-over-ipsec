4d074fcc7432bcaa7082679334a1661e5305be21
20eung <20eung@gmail.com>
2022-12-11 15:42:23 +0900

    add fg1-vpn

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,26 +7,26 @@ module "fg1-vpn" {
     ### IPsec VPN Phase1-interface 설정
     name                = "fg1-vpn"
     interface           = "wan1"
+    proposal_phase1     = "aes256-sha1"  # default = aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1
+    remote_gw           = "1.1.2.2"
+    psksecret	        = "PreSharedKey"
 #   ike_version         = "2"            # default = 1
 #   keylife             = "28800"        # default = 86400
 #   mode                = "main"         # default = main
 #   peertype            = "any"          # default = any
 #   net_device	        = "disable"      # default = disable
-    proposal_phase1     = "aes256-sha1"  # default = aes128-sha256 aes256-sha256 aes128-sha1 aes256-sha1
 #   dpd                 = "on-idle"      # default = on-demand  
 #   dhgrp_phase1        = "2"            # default = 14 5
 #   nattraversal        = "enable"       # default = enable
-    remote_gw           = "1.1.2.2"
-    psksecret	        = "PreSharedKey"
 #   dpd_retryinterval   = "10"           # default = 20
     
     ### IPsec VPN Phase2-interface 설정
     proposal_phase2     = "aes256-sha1"
+    auto_negotiate      = "enable"       # default = disable
 #   pfs	                = "enable"       # default = enable
 #   dhgrp_phase2        = "2"            # default = 14 5
 #   replay              = "enable"       # default = enable
 #   keepalive           = "disable"      # default = disable
-    auto_negotiate      = "enable"       # default = disable
 #   keylifeseconds      = "27000"        # default = 43200
 
     ### System Interface 설정
